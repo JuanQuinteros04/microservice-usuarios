802503eccacf2cb8b464547ef037214e083f5b16
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2023-07-12 16:17:16 -0300

    Adding Exceptions class

--- src/main/java/com/formacionbdi/springboot/app/usuarios/controller/UsuarioController.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/controller/UsuarioController.java
@@ -2,18 +2,24 @@ package com.formacionbdi.springboot.app.usuarios.controller;
 
 import com.formacionbdi.springboot.app.commons.usuarios.model.entity.Usuario;
 import com.formacionbdi.springboot.app.usuarios.service.UsuarioServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/users")
 @RestController
 public class UsuarioController {
 
     @Autowired
     UsuarioServiceImpl usuarioService;
 
-    @GetMapping("/listar")
+    Logger logger = LoggerFactory.getLogger(UsuarioController .class);
+
+
+    @GetMapping(value = "/listar")
     public List<Usuario> listar(){
         return usuarioService.findAll();
     }
